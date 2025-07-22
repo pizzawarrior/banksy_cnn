803bbefdeb6cfe7bac49260f7465db29272f4b89
pizzawarrior <i.norstad@gmail.com>
2025-07-22 10:31:25 -0700

    project init

---
+++ import_data.py
@@ -0,0 +1,6 @@
+'''
+we want to read in all images and add them to a single matrix.
+will need to immediately create corresponding labels.
+then we use np.random to pick training and testing images. labels are adjusted accordingly.
+returns a matrix of training images and training labels, and a matrix of testing images and testing labels
+'''
